f00081652b8e25efb779596ceb3456fb9ca13246
Docs Robot <robot@example.com>
2023-04-04 10:45:04 +0000

    Committing documentation changes

--- docs/class_button_and_l_e_d.js
+++ docs/class_button_and_l_e_d.js
@@ -1,7 +1,7 @@
 var class_button_and_l_e_d =
 [
     [ "ButtonAndLED", "class_button_and_l_e_d.html#a2bc530df268024719a2f8642cba6d003", null ],
-    [ "getBtnState", "class_button_and_l_e_d.html#aa2700d286f029cf9454c75df479a76e9", null ],
+    [ "getBtnState", "class_button_and_l_e_d.html#adc4af37ef234eb7b7b9d745547cd2187", null ],
     [ "initializeNative", "class_button_and_l_e_d.html#a337feea50e80de2c35951d1628b3b2ae", null ],
     [ "ledOff", "class_button_and_l_e_d.html#ae6577c71d6b27b2d5e1fb0b814881a61", null ],
     [ "ledOn", "class_button_and_l_e_d.html#a7a7e572e97bb70c3fe1a2abeccfef3fc", null ],
--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['getbtnstate_0',['getBtnState',['../class_button_and_l_e_d.html#aa2700d286f029cf9454c75df479a76e9',1,'ButtonAndLED']]]
+  ['getbtnstate_0',['getBtnState',['../class_button_and_l_e_d.html#adc4af37ef234eb7b7b9d745547cd2187',1,'ButtonAndLED']]]
 ];
